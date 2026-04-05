39c465a1403fa8ce8e64b04ef7beaf5d4bb57d8e
Your Name <Your Email>
2026-04-05 17:13:13 +0530

    first commit

---
+++ src/main/java/com/healthCare/FSAD/FsadApplication.java
@@ -0,0 +1,13 @@
+package com.healthCare.FSAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthCare/FSAD/FsadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthCare.FSAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
